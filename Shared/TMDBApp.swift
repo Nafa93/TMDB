a92be6ab8c89b34b88e68d58d3bd02f1aa58ef79
Nicolas Alejandro Fernandez Amorosino <nicolasfernandez93@gmail.com>
2022-03-15 12:16:16 -0300

    Initial Commit

---
+++ Shared/TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBApp.swift
+//  Shared
+//
+//  Created by Nicolas Alejandro Fernandez Amorosino on 15/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
